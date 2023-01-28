1ce46b7abe58d6df98c14ded606585ff184dfa00
Ajithakvp <72121496+Ajithakvp@users.noreply.github.com>
2023-01-28 12:40:40 +0530

    update

--- app/src/main/java/com/example/cistronuser/Common/ConnectionRecevier.java
+++ app/src/main/java/com/example/cistronuser/Common/ConnectionRecevier.java
@@ -1,25 +1,38 @@
 package com.example.cistronuser.Common;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.Dialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.net.ConnectivityManager;
 import android.net.Network;
 import android.net.NetworkInfo;
 import android.util.Log;
+import android.util.SparseArray;
 import android.view.LayoutInflater;
+import android.view.SurfaceHolder;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
+import androidx.core.app.ActivityCompat;
 
 import com.example.cistronuser.R;
+import com.example.cistronuser.ServiceEngineer.Activity.PendicallActivity;
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.io.IOException;
 
 public class ConnectionRecevier extends BroadcastReceiver {
 
@@ -56,5 +69,124 @@ public class ConnectionRecevier extends BroadcastReceiver {
         }
 
     }
+
+
+
+
+
+    // BarCodeView
+
+// SurfaceView surfaceView;
+//
+//    TextView txtBarcodeValue;
+//
+//    private BarcodeDetector barcodeDetector;
+//    private CameraSource cameraSource;
+//    private static final int REQUEST_CAMERA_PERMISSION = 201;
+//    Button btnAction;
+//    String intentData = "";
+    //   surfaceView=findViewById(R.id.surfaceView);
+    //        txtBarcodeValue=findViewById(R.id.txtBarcodeValue);
+    //        btnAction = findViewById(R.id.btnAction);
+    //
+    //        btnAction.setOnClickListener(new View.OnClickListener() {
+    //            @Override
+    //            public void onClick(View v) {
+    //                if (intentData.length() > 0) {
+    //                    startActivity(new Intent(Intent.ACTION_VIEW , Uri.parse(intentData)));
+    //                }
+    //            }
+    //        });
+
+//    private void initialiseDetectorsAndSources() {
+//
+//        // Toast.makeText(getApplicationContext() , "Barcode scanner started" , Toast.LENGTH_SHORT).show();
+//        barcodeDetector = new BarcodeDetector.Builder(this)
+//                .setBarcodeFormats(Barcode.ALL_FORMATS)
+//                .build();
+//
+//        cameraSource = new CameraSource.Builder(this , barcodeDetector)
+//                .setRequestedPreviewSize(1920 , 1080)
+//                .setAutoFocusEnabled(true) //you should add this feature
+//                .build();
+//
+//        surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
+//            @Override
+//            public void surfaceCreated(SurfaceHolder holder) {
+//                try {
+//                    if (ActivityCompat.checkSelfPermission(PendicallActivity.this , Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+//                        cameraSource.start(surfaceView.getHolder());
+//                    } else {
+//                        ActivityCompat.requestPermissions(PendicallActivity.this , new
+//                                String[]{Manifest.permission.CAMERA} , REQUEST_CAMERA_PERMISSION);
+//                    }
+//
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//
+//            @Override
+//            public void surfaceChanged(SurfaceHolder holder , int format , int width , int height) {
+//            }
+//
+//            @Override
+//            public void surfaceDestroyed(SurfaceHolder holder) {
+//                cameraSource.stop();
+//            }
+//        });
+//
+//
+//        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+//            @Override
+//            public void release() {
+//                //Toast.makeText(getApplicationContext() , "To prevent memory leaks barcode scanner has been stopped" , Toast.LENGTH_SHORT).show();
+//            }
+//
+//            @Override
+//            public void receiveDetections(Detector.Detections<Barcode> detections) {
+//                final SparseArray<Barcode> barcodes = detections.getDetectedItems();
+//                if (barcodes.size() != 0) {
+//                    txtBarcodeValue.post(new Runnable() {
+//                        @Override
+//                        public void run() {
+//                            // btnAction.setText("LAUNCH URL");
+//                            intentData = barcodes.valueAt(0).displayValue;
+//
+//                            Dialog dialog=new Dialog(PendicallActivity.this);
+//                            dialog.setContentView(R.layout.activity_main);
+//                            dialog.show();
+//                            TextView tvCode=dialog.findViewById(R.id.tvCode);
+//                            ImageView ivClose=dialog.findViewById(R.id.ivClose);
+//
+//                            tvCode.setText(intentData);
+//
+//
+//                            ivClose.setOnClickListener(new View.OnClickListener() {
+//                                @Override
+//                                public void onClick(View v) {
+//                                    dialog.dismiss();
+//                                }
+//                            });
+//                            // txtBarcodeValue.setText(intentData);
+//                        }
+//                    });
+//                }
+//            }
+//        });
+//    }
+//
+//
+//    @Override
+//    protected void onPause() {
+//        super.onPause();
+//        cameraSource.release();
+//    }
+//
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//        initialiseDetectorsAndSources();
+//    }
 
 }
--- app/src/main/java/com/example/cistronuser/ServiceEngineer/Activity/PendicallActivity.java
+++ app/src/main/java/com/example/cistronuser/ServiceEngineer/Activity/PendicallActivity.java
@@ -1,18 +1,42 @@
 package com.example.cistronuser.ServiceEngineer.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.Camera;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.lifecycle.LifecycleOwner;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Size;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.cistronuser.R;
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.util.concurrent.ExecutionException;
 
 public class PendicallActivity extends AppCompatActivity {
 
     RecyclerView rvPendingCall;
     ImageView ivBack;
+
+
+
+
 
     @SuppressLint("MissingInflatedId")
     @Override
