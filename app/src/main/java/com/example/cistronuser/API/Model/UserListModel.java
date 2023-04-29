8ee94d1dff05cd59ee3c3d97cd210339f799c002
Ajithakvp <72121496+Ajithakvp@users.noreply.github.com>
2023-04-29 11:10:35 +0530

    update

--- app/src/main/java/com/example/cistronuser/API/Model/UserListModel.java
+++ app/src/main/java/com/example/cistronuser/API/Model/UserListModel.java
@@ -9,6 +9,18 @@ public class UserListModel {
     //      "name": "Santhosh",
     //      "photo":
 
+
+    // "empid": "e339",
+    //      "name": "Kannan N",
+    //      "Designation": "Service Engineer",
+    //      "Mobile no": "7540042073",
+    //      "DOJ": "2020-10-19",
+    //      "DOB": "1993-12-16",
+    //      "Email": "gomathi@cistronsystems.com",
+    //      "Branch": "Tamilnadu",
+    //      "photo":
+    //       Company
+
     @SerializedName("empid")
     private String empid;
 
@@ -18,8 +30,76 @@ public class UserListModel {
     @SerializedName("Designation")
     private String Designation;
 
+    @SerializedName("Mobile no")
+    private String Mobileno;
+
+    @SerializedName("DOJ")
+    private String DOJ;
+
+    @SerializedName("DOB")
+    private String DOB;
+
+    @SerializedName("Email")
+    private String Email;
+
+    @SerializedName("Company")
+    private String Company;
+
+    @SerializedName("Branch")
+    private String Branch;
+
     @SerializedName("photo")
     private String photo;
+
+    public String getCompany() {
+        return Company;
+    }
+
+    public void setCompany(String company) {
+        Company = company;
+    }
+
+    public String getMobileno() {
+        return Mobileno;
+    }
+
+
+
+    public void setMobileno(String mobileno) {
+        Mobileno = mobileno;
+    }
+
+    public String getDOJ() {
+        return DOJ;
+    }
+
+    public void setDOJ(String DOJ) {
+        this.DOJ = DOJ;
+    }
+
+    public String getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(String DOB) {
+        this.DOB = DOB;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getBranch() {
+        return Branch;
+    }
+
+    public void setBranch(String branch) {
+        Branch = branch;
+    }
 
     public String getEmpid() {
         return empid;
@@ -52,4 +132,6 @@ public class UserListModel {
     public void setPhoto(String photo) {
         this.photo = photo;
     }
+
+
 }
--- app/src/main/java/com/example/cistronuser/ServiceEngineer/Adapter/UserListAdapter.java
+++ app/src/main/java/com/example/cistronuser/ServiceEngineer/Adapter/UserListAdapter.java
@@ -1,17 +1,21 @@
 package com.example.cistronuser.ServiceEngineer.Adapter;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.util.Base64;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.cistronuser.API.Model.MyStockListSEModel;
@@ -34,6 +38,8 @@ public class UserListAdapter extends RecyclerView.Adapter<UserListAdapter.ViewHo
         this.userListModels = userListModels;
     }
 
+    boolean isClick=true;
+
     @NonNull
     @Override
     public UserListAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -45,20 +51,94 @@ public class UserListAdapter extends RecyclerView.Adapter<UserListAdapter.ViewHo
     public void onBindViewHolder(@NonNull UserListAdapter.ViewHolder holder, int position) {
 
 
-        holder.tvEmpId.setText( userListModels.get(position).getEmpid());
+        holder.tvEmpId.setText(userListModels.get(position).getEmpid());
         holder.tvEmpName.setText(userListModels.get(position).getName());
         holder.tvDesignation.setText(userListModels.get(position).getDesignation());
-
         String photo = userListModels.get(position).getPhoto();
+        holder.tvEmail.setText(userListModels.get(position).getEmail());
+        holder.tvBranch.setText(userListModels.get(position).getBranch());
+        holder.tvDob.setText(userListModels.get(position).getDOB());
+        holder.tvDoj.setText(userListModels.get(position).getDOJ());
+        holder.tvMobile.setText(userListModels.get(position).getMobileno());
+        String MobileNo=userListModels.get(position).getMobileno();
+        String Email=userListModels.get(position).getEmail();
+
+        if (userListModels.get(position).getCompany().trim().equals("1")){
+            holder.tvCistronCompany.setVisibility(View.VISIBLE);
+            holder.tvSukCompany.setVisibility(View.GONE);
+        }else {
+            holder.tvCistronCompany.setVisibility(View.GONE);
+            holder.tvSukCompany.setVisibility(View.VISIBLE);
+        }
 
         try {
-            byte[] decodedString = Base64.decode(photo, Base64.DEFAULT);
-            Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
-            holder.ivProfile.setImageBitmap(decodedByte);
-        } catch (Exception e) {
-            e.printStackTrace();
+            if (MobileNo.trim().equals("") || MobileNo.trim().equals(null)) {
+                holder.tvMobile.setText("Not Available");
+                holder.ivCall.setVisibility(View.INVISIBLE);
+            } else {
+                holder.ivCall.setVisibility(View.VISIBLE);
+            }
+        }catch (Exception e){
+
+        }
+
+        if (Email.trim().equals("") || Email.trim().equals(null)){
+            holder.tvEmail.setText("Not Available");
+            holder.ivMail.setVisibility(View.INVISIBLE);
+        }else {
+            holder.ivMail.setVisibility(View.VISIBLE);
+        }
+
+        holder.ivMail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("mailto:" +Email ));
+                activity.startActivity(intent);
+
+            }
+        });
+
+
+        holder.cvView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isClick==true){
+                    holder.rlDetails.setVisibility(View.VISIBLE);
+                    isClick=false;
+                }else {
+                    isClick=true;
+                    holder.rlDetails.setVisibility(View.GONE);
+                }
+
+            }
+        });
+
+        holder.ivCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:" + MobileNo));
+                activity.startActivity(intent);
+            }
+        });
+
+
+        if (photo.trim().equals("") || photo.trim().equals(null)) {
+            holder.ivProfile.setImageResource(R.drawable.user);
+        } else {
+
+            try {
+                byte[] decodedString = Base64.decode(photo, Base64.DEFAULT);
+                Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length);
+                holder.ivProfile.setImageBitmap(decodedByte);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
+
+
 
     public void filter(String charText) {
         charText = charText.toLowerCase();
@@ -103,16 +183,30 @@ public class UserListAdapter extends RecyclerView.Adapter<UserListAdapter.ViewHo
 
     public class ViewHolder extends RecyclerView.ViewHolder {
 
-        TextView tvEmpName, tvEmpId, tvDesignation;
+        TextView tvEmpName, tvEmpId, tvDesignation,tvEmail,tvMobile,tvDob,tvDoj,tvBranch,tvCistronCompany,tvSukCompany;
         CircleImageView ivProfile;
+        ImageView ivCall,ivMail;
+        RelativeLayout rlDetails;
+        CardView cvView;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-
+            cvView = itemView.findViewById(R.id.cvView);
+            rlDetails = itemView.findViewById(R.id.rlDetails);
             tvEmpName = itemView.findViewById(R.id.tvEmpName);
             tvEmpId = itemView.findViewById(R.id.tvEmpId);
             tvDesignation = itemView.findViewById(R.id.tvDesignation);
             ivProfile = itemView.findViewById(R.id.ivProfile);
+            tvEmail = itemView.findViewById(R.id.tvEmail);
+            tvMobile = itemView.findViewById(R.id.tvMobile);
+            tvDob = itemView.findViewById(R.id.tvDob);
+            tvDoj = itemView.findViewById(R.id.tvDoj);
+            tvBranch = itemView.findViewById(R.id.tvBranch);
+            ivCall = itemView.findViewById(R.id.ivCall);
+            ivMail=itemView.findViewById(R.id.ivMail);
+            tvCistronCompany = itemView.findViewById(R.id.tvCistronCompany);
+            tvSukCompany=itemView.findViewById(R.id.tvSukCompany);
+
 
         }
     }
